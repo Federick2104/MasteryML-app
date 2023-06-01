4af1ec282e453b7c88b98b8b722b1175a3811b14
Federico Schianchi <federicoschianchi2004@gmail.com>
2023-06-01 09:36:51 +0200

    end all

--- screens/ChatCommunity.js
+++ screens/ChatCommunity.js
@@ -18,23 +18,35 @@ const ChatCommunity = () => {
   }
 
   useLayoutEffect(() => {
+    // Quando questo effetto viene eseguito, aggiorna le opzioni dell'intestazione del componente di navigazione.
     navigation.setOptions({
+      // Imposta le opzioni dell'intestazione del componente di navigazione.
       headerRight: () => (
+        // Componente personalizzato da visualizzare sul lato destro dell'intestazione.
         <TouchableOpacity style={{marginRight: 10}} onPress={handleSignOut}>
           <AntDesign name="logout" size={24} color="black" style={{marginRight: 10}} />
         </TouchableOpacity>
       )
     })
   }, [navigation])
-
+  
   useLayoutEffect(() => {
+    // Quando questo effetto viene eseguito, configura un ascoltatore in tempo reale per la collezione 'chats'.
     const collectionRef = collection(database, 'chats')
+    // Ottieni un riferimento alla collezione 'chats' nel database.
+  
     const q = query(collectionRef, orderBy('createdAt', 'desc'))
-
+    // Crea una query che ordina i documenti della collezione per il campo 'createdAt' in ordine decrescente.
+  
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      console.log('Total users: ', querySnapshot.size);
+      // Configura un ascoltatore per la query che si attiva ogni volta che ci sono modifiche nella collezione.
+  
+      console.log('Tot Users: ', querySnapshot.size);
+  
       setMessages(
         querySnapshot.docs.map((doc) => ({
+          // Aggiorna lo stato 'messages' con i dati dei documenti nell'istantanea della query.
+  
           _id: doc.data()._id,
           createdAt: doc.data().createdAt.toDate(),
           text: doc.data().text,
@@ -42,24 +54,36 @@ const ChatCommunity = () => {
         }))
       )
     })
+    // La funzione onSnapshot ascolta le modifiche nella query ed esegue la funzione di callback fornita.
+  
     return () => unsubscribe()
+    // Restituisce una funzione di pulizia che annulla l'ascoltatore quando il componente viene smontato o l'effetto viene eseguito nuovamente.
+  
   }, [])
+  // L'effetto viene eseguito solo una volta quando il componente viene montato poiché ha un array di dipendenze vuoto.
+  
   
   const onSend = useCallback((messages = []) => {
-
+    // Definisce una funzione di callback chiamata 'onSend' utilizzando il hook 'useCallback'.
+  
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-
+    // Aggiorna lo stato 'messages' aggiungendo i nuovi 'messages' ai messaggi precedenti utilizzando il metodo 'GiftedChat.append'.
+  
     const { _id, createdAt, text, user } = messages[0]
-
+    // Estrapola le proprietà del primo messaggio nell'array 'messages'.
+  
     addDoc(collection(database, 'chats'), {
+      // Aggiunge un nuovo documento alla collezione 'chats' nel database.
       _id,
       createdAt,
       text,
       user
     })
+    // Il documento contiene le proprietà estratte dal primo messaggio.
+  
   }, [])
-
-
+  // La funzione 'onSend' viene creata una volta e non dipende da variabili esterne o props.
+  
 
   return (
     <GiftedChat
@@ -69,8 +93,6 @@ const ChatCommunity = () => {
         _id: auth?.currentUser?.email,
         avatar: 'https://i.pravatar.cc/300',
         name: auth?.currentUser?.email,
-        
-
       }}
       messagesContainerStyle={{
         backgroundColor: 'white',
