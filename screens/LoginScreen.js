06e32adbf4e4a8e03438e5e5d2b788022e47e43a
Federico Schianchi <federicoschianchi2004@gmail.com>
2023-06-11 13:46:23 +0200

    last push

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -61,9 +61,6 @@ const LoginScreen = () => {
                     <TouchableOpacity onPress={() => navigation.navigate('SignUp')} >
                         <Text style={{color: 'yellow', fontWeight: 'bold', fontSize: 16}}> Sign Up</Text>
                     </TouchableOpacity>
-                    
-                   
-                    
                 </View>
         </View>
         <View style={styles.support}>
